f7dab0bdad2fc6200db979beb7d42370c87d68a9
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-02-03 16:38:23 +0200

    Add basic page app bar

---
+++ client/src/components/AppBar/AppBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Layout, Menu } from 'antd';
+import './header.css';
+
+const AppBar = () => {
+  const { Header } = Layout;
+
+  return (
+    <Layout>
+      <Header className="header" >
+        <div className="logo" style={{color: 'white'}}>
+          <Link to="/">
+            HOME
+          </Link>
+        </div>
+        <Menu theme="dark" mode="horizontal" >
+          <Menu.Item key="1">
+            <Link to="/login">Login</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/register">Register</Link></Menu.Item>
+        </Menu>
+      </Header>
+    </Layout>
+
+  );
+};
+
+export default AppBar;
