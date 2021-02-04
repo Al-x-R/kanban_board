71ad8df699c5758f967ee1088a0806925b0625c4
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-02-04 21:51:42 +0200

    Connect redux store to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './router';
-import 'antd/dist/antd.css';
 import './App.css';
 
 function App() {
